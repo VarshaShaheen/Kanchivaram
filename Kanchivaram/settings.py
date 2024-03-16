41d209848e4d00de255974f4ddf8ae2dd6cc659d
VarshaShaheen <varshashaheen2003@gmail.com>
2024-03-16 14:43:43 +0530

    to not learn .gitingore

--- Kanchivaram/settings.py
+++ Kanchivaram/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'compressor',
 
 ]
 
@@ -132,3 +133,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+COMPRESS_ROOT = BASE_DIR / 'static'
+
+COMPRESS_ENABLED = True
+
+STATICFILES_FINDERS = ('compressor.finders.CompressorFinder',)
