21b6cb2631d72326a5658a737fad141b994510bb
VarshaShaheen <varshashaheen2003@gmail.com>
2024-03-17 00:10:38 +0530

    price category page added

--- app/urls.py
+++ app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, price_category
 
 urlpatterns = [
     path('', index, name='index'),
+    path('price-range/<int:a>/<int:b>/', price_category, name='price_category'),
 ]
--- app/views.py
+++ app/views.py
@@ -6,3 +6,8 @@ def index(request):
     categories = Category.objects.all()
     products = Product.objects.all()
     return render(request, 'app/index.html', {'categories': categories, 'products': products})
+
+
+def price_category(request, a, b):
+    products_in_range = Product.objects.filter(mrp__gte=a, mrp__lte=b)
+    return render(request,'app/price_category/price_category.html', {'products': products_in_range})
