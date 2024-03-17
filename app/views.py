6c5fec5b957cc7c8461aac36f5ae42d00a7ed381
VarshaShaheen <varshashaheen2003@gmail.com>
2024-03-17 19:46:11 +0530

    cart added

--- app/views.py
+++ app/views.py
@@ -24,20 +24,21 @@ def categories(request, category_slug):
 
 def view_cart(request):
     cart_items = CartItem.objects.filter(user=request.user)
-    total_price = sum(item.product.price * item.quantity for item in cart_items)
+    total_price = sum(item.product.mrp for item in cart_items)
     return render(request, 'app/cart/cart.html', {'cart_items': cart_items, 'total_price': total_price})
 
 
+@login_required(login_url='/login/')
 def add_to_cart(request, product_id):
     product = Product.objects.get(id=product_id)
     cart_item, created = CartItem.objects.get_or_create(product=product,
                                                         user=request.user)
-    cart_item.quantity += 1
     cart_item.save()
-    return redirect('cart:view_cart')
+    return redirect('view_cart')
 
 
+@login_required(login_url='/login/')
 def remove_from_cart(request, item_id):
     cart_item = CartItem.objects.get(id=item_id)
     cart_item.delete()
-    return redirect('cart:view_cart')
+    return redirect('view_cart')
