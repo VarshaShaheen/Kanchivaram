bb9c614801767b6b6a8fd121b79f78781ac85668
VarshaShaheen <varshashaheen2003@gmail.com>
2024-03-19 00:00:04 +0530

    product page added

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import index, price_category, categories, remove_from_cart, add_to_cart, view_cart
+from .views import index, price_category, categories, remove_from_cart, add_to_cart, view_cart, product_detail
 
 urlpatterns = [
     path('category/<slug:category_slug>/', categories, name='category'),
@@ -8,5 +8,6 @@ urlpatterns = [
     path('cart/', view_cart, name='view_cart'),
     path('add/<int:product_id>/', add_to_cart, name='add_to_cart'),
     path('remove/<int:item_id>/', remove_from_cart, name='remove_from_cart'),
+    path('product/<str:product_code>/', product_detail, name='product_detail'),
 
 ]
