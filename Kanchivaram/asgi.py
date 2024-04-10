1c36d3cca7f4018bd56b8717ce3c3fa51b8e6d42
VarshaShaheen <varshashaheen2003@gmail.com>
2024-04-10 23:20:40 +0530

    added running script

--- Kanchivaram/asgi.py
+++ Kanchivaram/asgi.py
@@ -6,16 +6,6 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
 """
-
-import os
-
 from django.core.asgi import get_asgi_application
-import dotenv
-
-dotenv.load_dotenv(
-    os.path.join(os.path.dirname(os.path.dirname(__file__)), '.env')
-)
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Kanchivaram.settings')
 
 application = get_asgi_application()
