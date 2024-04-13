86619b9d43cbb942097260abcb2bd7cf8ffe4e40
VarshaShaheen <varshashaheen2003@gmail.com>
2024-04-14 00:28:24 +0530

    cleaned

--- payment/admin.py
+++ payment/admin.py
@@ -3,16 +3,15 @@ from django.contrib import admin
 from payment.models import Payment, Address
 from app.models import Order
 
-
 # Register your models here.
 admin.site.register(Order)
 admin.site.register(Address)
 
+
 @admin.register(Payment)
 class PaymentAdmin(admin.ModelAdmin):
-    fields = ['id', 'amount', 'currency', 'user', 'status','cart_items',]
+    fields = ['id', 'amount', 'currency', 'user', 'status', 'cart_items', ]
     list_display = ['id', 'amount', 'currency', 'user', 'status', ]
-    list_filter = ['status',]
-    search_fields = ['id', 'user__email','amount' ]
+    list_filter = ['status', ]
+    search_fields = ['id', 'user__email', 'amount']
     readonly_fields = ['id']
-
